ebfa6da7cdf3c21017791cd2c7ae172cef9a0197
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-09-07 23:22:42 -0300

    chore: conserta formatação em NofFoundException

--- src/main/java/edu/fafic/msstock/api/error/NotFoundException.java
+++ src/main/java/edu/fafic/msstock/api/error/NotFoundException.java
@@ -1,7 +1,7 @@
 package edu.fafic.msstock.api.error;
 
 public class NotFoundException extends RuntimeException {
-    
+
     public NotFoundException(String message) {
         super(message);
     }
