52f38e07bc5ed91d8baa444c0ee28260d019cf25
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-09-07 23:33:58 -0300

    fix: adiciona MapStruct annotation processor às dependências

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
