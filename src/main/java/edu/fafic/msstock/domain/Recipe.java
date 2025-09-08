6ea324a3de078314d909b727e46835ba5436d04f
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-09-08 00:41:01 -0300

    fix: conserta parâmetro collation confundido com collection no documento Recipe

--- src/main/java/edu/fafic/msstock/domain/Recipe.java
+++ src/main/java/edu/fafic/msstock/domain/Recipe.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Data
-@Document(collation = "recipes")
+@Document(collection = "recipes")
 public class Recipe {
 
     @Id
