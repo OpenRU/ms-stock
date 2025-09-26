a484fd91fd5f371ed3ac46c2fbeb46baf390b235
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-09-25 22:45:00 -0300

    feat: implementa serviço de autenticação para validar o token

---
+++ src/main/java/edu/fafic/msstock/infrastructure/service/AuthService.java
@@ -0,0 +1,47 @@
+package edu.fafic.msstock.infrastructure.service;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final RestClient http;
+    private final HttpServletRequest request;
+
+    private final String MS_AUTH = "/lonlon-api"; // TODO: Inserir o endpoint da API de autenticação
+    private final String VALIDATION_ENDPOINT = "/validate-token";
+
+    public boolean validateToken() {
+        String token = request.getHeader("Authorization");
+
+        if (token == null || token.isBlank()) {
+            return false;
+        }
+
+        String uri = MS_AUTH + VALIDATION_ENDPOINT;
+        Map<String, String> body = Map.of("token", token);
+
+        try {
+            HttpStatusCode status = http.post()
+                    .uri(uri)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(body)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .getStatusCode();
+
+            return status.is2xxSuccessful();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
